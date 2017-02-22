86f4177734c8eca7eda2c6bae195e72699902acf
ATPIAD1\atp3rxk <rkatipally@atpco.net>
2017-02-22 08:00:23 -0500

    Initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('adminApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
